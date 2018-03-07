d43aeb246de8901f34df8afbf954ebf1f587d016
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2018-03-07 17:02:24 +0400

    Added modifications to the template: now all available algorithms are displayed below

--- main.py
+++ main.py
@@ -22,13 +22,15 @@ def algorithm_factory(checking_method):
 
 
 def apply_all_available_similarity_checking_methods(text_a, text_b):
-    list = ["cosine", "jaccard", "cosine_stemming", "jaccard_stemming", "cosine_stemming_stopwords_removed",
+    result_list = list()
+    methods = ["cosine", "jaccard", "cosine_stemming", "jaccard_stemming", "cosine_stemming_stopwords_removed",
             "jaccard_stemming_stopwords_removed"]
-    for i in list:
+    for i in methods:
         algorithm = algorithm_factory(i)
         result = algorithm.compare(text_a, text_b)
         print(i + " " + str(result))
-
+        result_list.append((i, str(result)))
+    return result_list
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -42,10 +44,10 @@ def index():
         algorithm = algorithm_factory(checking_method)
         result = algorithm.compare(text_a, text_b)
 
-        apply_all_available_similarity_checking_methods(text_a, text_b)
+        result_list = apply_all_available_similarity_checking_methods(text_a, text_b)
 
         return render_template("result.html", text_a=text_a, text_b=text_b, checking_method=checking_method.upper(),
-                               result=result)
+                               result=result, result_list = result_list)
 
 
 if __name__ == "__main__":
