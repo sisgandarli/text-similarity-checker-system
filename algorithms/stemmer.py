857dc409df3fe56957b0574f129798acadccd6ec
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2018-03-14 03:34:36 +0400

    Stemmer was modified

--- algorithms/stemmer.py
+++ algorithms/stemmer.py
@@ -1,7 +1,7 @@
 # Stemmer class definition.
 class Stemmer:
     # Stores the words loaded from the words.txt file.
-    words = []
+    words = set()
     # Stores the suffixes loaded from the suffix.txt file.
     suffixes = []
 
@@ -21,34 +21,42 @@ class Stemmer:
     # Loads the words from the word.txt file into memory.
     def load_words(self):
         # Open words.txt file in read mode with utf-8 encoding.
-        with open("files/words.txt", "r", encoding="utf8") as words_file:
+        with open("words.txt", "r", encoding="utf8") as words_file:
             # Iterate over each line in the words.txt file
             for word in words_file:
                 # Trim the spaces and newline characters from the string before adding to the list.
-                self.words.append(word.strip())
+                self.words.add(word.strip())
 
     # Loads the suffixes from the suffix.txt file into memory.
     def load_suffixes(self):
         # Open suffix.txt file in read mode with utf-8 encoding.
-        with open("files/suffix.txt", "r", encoding="utf8") as suffix_file:
+        with open("suffix.txt", "r", encoding="utf8") as suffix_file:
             # Iterate over each line in the suffix.txt file.
             for suffix in suffix_file:
                 # Trim the spaces and newline characters from the string before adding to the list.
                 self.suffixes.append(suffix.strip())
 
-    # Returns the stemmed version of word.
-    def stem_word(self, word):
-        # If the word is already in the database, return it.
-        if word in self.words:
-            return word
+    # Removes one suffix at a time
+    def suffix(self, word):
         # Iterate over the suffixes.
         for suffix in self.suffixes:
             # If the word ends with the particular suffix, create a new word by removing that suffix.
             if word.endswith(suffix):
-                new_word = word[:word.rfind(suffix)]
-                # If new word is in the database, return it.
-                if new_word in self.words:
-                    return new_word
+                word = word[:word.rfind(suffix)]
+                break
+        return word
+
+    # Returns the stemmed version of word.
+    def stem_word(self, word):
+        # Change the word to lowercase.
+        word = word.lower()
+        # Remove suffixes until word is in dictionary
+        while word not in self.words:
+            new_word = self.suffix(word)
+            if new_word != word:
+                word = new_word
+            else:
+                break
         # If it is not possible to apply stemming to that word, return it.
         return word
 
