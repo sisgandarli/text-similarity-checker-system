c74904c93b4140e259f85c001f62744dae2ea521
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2018-03-29 13:39:12 +0400

    Modified the factory method

--- main.py
+++ main.py
@@ -2,37 +2,70 @@ from flask import Flask, render_template, request
 from algorithms.similarity_algorithms import *
 from algorithms.cosine import *
 from algorithms.jaccard import *
+from algorithms.dice import *
+from algorithms.euclidean import *
+from algorithms.matching import *
+from algorithms.overlap import *
 
 app = Flask(__name__)
 
 
 def algorithm_factory(checking_method):
-    if checking_method == "cosine":
-        return CosineSimilarityAlgorithm()
-    elif checking_method == "jaccard":
-        return JaccardSimilarityAlgorithm()
-    elif checking_method == "cosine_stemming":
-        return CosineSimilarityAlgorithmWithStemming()
-    elif checking_method == "jaccard_stemming":
-        return JaccardSimilarityAlgorithmWithStemming()
-    elif checking_method == "cosine_stemming_stopwords_removed":
-        return CosineSimilarityAlgorithmWithStemmingStopwordsRemoved()
-    elif checking_method == "jaccard_stemming_stopwords_removed":
-        return JaccardSimilarityAlgorithmWithStemmingStopwordsRemoved()
+    if checking_method is None:
+        return None
+    elif checking_method == "cos":
+        return CosineSim()
+    elif checking_method == "cos_stem":
+        return CosineSimStem()
+    elif checking_method == "cos_stem_stop_rem":
+        return CosineSimStemStopwordsRem()
+    elif checking_method == "jac":
+        return JaccardSim()
+    elif checking_method == "jac_stem":
+        return JaccardSimStem()
+    elif checking_method == "jac_stem_stop_rem":
+        return JaccardSimStemStopwordsRem()
+    elif checking_method == "dice":
+        return DiceSim()
+    elif checking_method == "dice_stem":
+        return DiceSimStem()
+    elif checking_method == "dice_stem_stop_rem":
+        return DiceSimStemStopwordsRem()
+    elif checking_method == "eucl":
+        return EuclideanSim()
+    elif checking_method == "eucl_stem":
+        return EuclideanSimStem()
+    elif checking_method == "eucl_stem_stop_rem":
+        return EuclideanSimStemStopwordsRem()
+    elif checking_method == "match":
+        return MatchingCoefSim()
+    elif checking_method == "match_stem":
+        return MatchingCoefStem()
+    elif checking_method == "match_stem_stop_rem":
+        return MatchingCoefSimStemStopwordsRem()
+    elif checking_method == "overlap":
+        return OverlapCoefSim()
+    elif checking_method == "overlap_stem":
+        return OverlapCoefStem()
+    elif checking_method == "overlap_stem_stop_rem":
+        return OverlapCoefSimStemStopwordsRem()
     else:
         return None
 
 
 def apply_all_available_similarity_checking_methods(text_a, text_b):
     result_list = list()
-    methods = ["cosine", "jaccard", "cosine_stemming", "jaccard_stemming", "cosine_stemming_stopwords_removed",
-            "jaccard_stemming_stopwords_removed"]
+    methods = ["cos", "cos_stem", "cos_stem_stop_rem", "jac", "jac_stem", "jac_stem_stop_rem", "dice", "dice_stem",
+               "dice_stem_stop_rem",
+               "eucl", "eucl_stem", "eucl_stem_stop_rem", "match", "match_stem", "match_stem_stop_rem", "overlap",
+               "overlap_stem", "overlap_stem_stop_rem"]
     for i in methods:
         algorithm = algorithm_factory(i)
         result = algorithm.compare(text_a, text_b)
         print(i + " " + str(result))
         result_list.append((i, str(result)))
     return result_list
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -49,7 +82,7 @@ def index():
         result_list = apply_all_available_similarity_checking_methods(text_a, text_b)
 
         return render_template("result.html", text_a=text_a, text_b=text_b, checking_method=checking_method.upper(),
-                               result=result, result_list = result_list)
+                               result=result, result_list=result_list)
 
 
 if __name__ == "__main__":
